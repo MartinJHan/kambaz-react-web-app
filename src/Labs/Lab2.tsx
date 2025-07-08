d0d4eced4db65a7ae54d6723b21a28299b65c28a
Martin Han <martinhan@Mac-2.lan>
2025-07-08 13:39:15 -0700

    first commit

---
+++ src/Labs/Lab2.tsx
@@ -0,0 +1,9 @@
+export default function Lab2() {
+  return (
+    <div>
+      <h3>Lab 2</h3>
+      <p>Welcome to Lab 2!</p>
+      <p>This lab component builds on the first one.</p>
+    </div>
+  );
+}
